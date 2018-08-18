bca554eb9bfd0e792b30df301d8e776d51bdc6c6
May Chen <maychen@Mays-MacBook-Pro.local>
2018-08-18 10:29:05 -1000

    Refactored code, removed unnecessary code

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -10,35 +10,29 @@ function _sumFibs(maxFibValue) {
   var sum = 0;
 
   // do your work here
-  //let evenNums = [2];
   let fibNums = [1, 2];
 
+  //Iterate through the fibNum array and add more fib numbers until the fib number is equal to the maxFibValue provided
   for (let i = 0; fibNums[fibNums.length - 1] !== maxFibValue; i++) {
     let fib = fibNums[i] + fibNums[i + 1];
     console.log("fib:", fib);
     fibNums.push(fib);
     console.log("Check fibNums:", fibNums);
-    // console.log("Check last element:", fibNums[fibNums.length - 1]);
-    // if (fib % 2 === 0) {
-    //   evenNums.push(fib);
-    //   console.log("Check evenNums:", evenNums);
-    // }
   }
-  // for (j = 0; j < evenNums.length; j++) {
-  //   sum += evenNums[j];
-  // }
 
-
+  //Function to for filter method to filter out even numbers
   getEvens = element => {
+    //return a new array with all elements that pass true test
     return !(element % 2);
   }
+  //From the fibNum array, filter out all the even numbers
   let evenNums = fibNums.filter(getEvens);
   console.log("\nevenNum arr: ", evenNums);
 
+  //Function to add all values in the callback function. Store the sum
   sumEvens = (prevValue, currentValue) => {
     return prevValue + currentValue;
   }
-
   sum = evenNums.reduce(sumEvens);
   console.log("Sum: ", sum);
 
